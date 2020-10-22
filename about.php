0a7bba2be7648a4727d514f19f839458f6bd1fb8
LaurynasG <glaurynasg@gmail.com>
2020-10-22 15:08:03 +0300

    added the about page

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php 
+    print 'this is the about page yes';
+    print 'very nice';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="about_style.css">
+</head>
+<body>
+    <div>
+        ABOUT PAGE
+    </div>
+</body>
+</html>
